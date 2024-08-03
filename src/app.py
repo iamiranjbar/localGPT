72ca80b910564467f5087042fdb67312101df595
Amir Ranjbar <iamirranjbar@gmail.com>
2024-08-03 21:20:46 +0330

    Add base code for question answering.

---
+++ src/app.py
@@ -0,0 +1,73 @@
+import streamlit as st
+# from langchain_community.llms import GPT4All
+from langchain_community.llms import LlamaCpp
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+
+MODEL_PATH = "/Users/amirranjbar/Desktop/Military/Code/models/mistral-7b-openorca.gguf"
+# llm = GPT4All(model=MODEL_PATH)
+n_gpu_layers = 1  # Metal set to 1 is enough.
+n_batch = 256  # Should be between 1 and n_ctx, consider the amount of RAM of your Apple Silicon Chip.
+
+# Make sure the model path is correct for your system!
+llm = LlamaCpp(
+    model_path="/Users/amirranjbar/Desktop/Military/Code/models/llama-2-7b-chat.Q2_K.gguf",
+    n_gpu_layers=n_gpu_layers,
+    n_batch=n_batch,
+    n_ctx=2048,
+    f16_kv=True,  # MUST set to True, otherwise you will run into problem after a couple of calls
+    verbose=True,
+)
+
+st.set_page_config(page_title="Streaming bot", page_icon="🤖")
+st.title("Streaming bot")
+
+def get_response(user_query, chat_history):
+
+    template = """
+    You are a helpful assistant. Answer the following questions considering the history of the conversation:
+
+    Chat history: {chat_history}
+
+    User question: {user_question}
+    """
+
+    prompt = ChatPromptTemplate.from_template(template)
+        
+    chain = prompt | llm | StrOutputParser()
+    
+    return chain.stream({
+        "chat_history": chat_history,
+        "user_question": user_query,
+    })
+
+# session state
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        AIMessage(content="Hello, I am a bot. How can I help you?"),
+    ]
+
+    
+# conversation
+for message in st.session_state.chat_history:
+    if isinstance(message, AIMessage):
+        with st.chat_message("AI"):
+            st.write(message.content)
+    elif isinstance(message, HumanMessage):
+        with st.chat_message("Human"):
+            st.write(message.content)
+
+# user input
+user_query = st.chat_input("Type your message here...")
+if user_query is not None and user_query != "":
+    st.session_state.chat_history.append(HumanMessage(content=user_query))
+
+    with st.chat_message("Human"):
+        st.markdown(user_query)
+
+    with st.chat_message("AI"):
+        response = st.write_stream(get_response(user_query, st.session_state.chat_history))
+
+    st.session_state.chat_history.append(AIMessage(content=response))
