0b1d40befc91c99dc63dff711493969080d93685
Amir Ranjbar <iamirranjbar@gmail.com>
2024-08-03 22:12:33 +0330

    Modify settings.

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,5 @@
 MODEL_PATH = "/Users/amirranjbar/Desktop/Military/Code/models/mistral-7b-openorca.gguf"
-# "/Users/amirranjbar/Desktop/Military/Code/models/llama-2-7b-chat.Q2_K.gguf"
+# MODEL_PATH = "/Users/amirranjbar/Desktop/Military/Code/models/llama-2-7b-chat.Q2_K.gguf"
 n_gpu_layers = 1  # Metal set to 1 is enough.
 n_batch = 256  # Should be between 1 and n_ctx, consider the amount of RAM of your Apple Silicon Chip.
 
